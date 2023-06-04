878cf40893cb23d197aec8b270d23b7b72494320
franAlcolea <fran@gmail.com>
2023-06-04 10:50:10 +0200

    Fichero añadido, proyecto de Netbeans

---
+++ src/main/java/com/mycompany/alcoleagarrido_git/AlcoleaGarrido_GIT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.alcoleagarrido_git;
+
+/**
+ *
+ * @author Fran
+ */
+public class AlcoleaGarrido_GIT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
